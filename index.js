d1bad3ad48d27f437a4c4836ff01880c5ae731c1
jctheking <93554396+jcthekin12345@users.noreply.github.com>
2024-01-16 22:08:48 +0200

    modified:   index.html new file:   index.js modified:   style.css

---
+++ index.js
@@ -0,0 +1,10 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the value it contains
+
+// 1. declaration   let x;
+// 2. assignment    x = 100;
+
+let age = 25;
+let price = 10.99;
+
+console.log(price);
